0cc347943d6055b2b84c25cc34ab3b38e76b2d8e
taylor <sptaylor56@gmail.com>
2019-01-30 15:33:46 -0800

    fixed typo

--- parse_kaggle.py
+++ parse_kaggle.py
@@ -11,13 +11,14 @@ def add_position_and_employment_status(df, company_filter=None):
     df['current_employee'] = df['job-title'].str.split('-').str[0]
     df['position'] = df['job-title'].str.split('-').str[1]
     if company_filter:
-        return reviews[reviews['company'] == company_filter]
+        return df[df['company'] == company_filter]
     else:
         return df[['company', 'summary', 'current_employee', 'position', 'pros', 'cons', 'advice-to-mgmt',
-               'overall-ratings', 'work-balance-stars', 'culture-values-stars', 'comp-benefit-stars',
-               'senior-mangemnet-stars']]
+                   'overall-ratings', 'work-balance-stars', 'culture-values-stars', 'comp-benefit-stars',
+                   'senior-mangemnet-stars']]
 
 
 if __name__ == "__main__":
     reviews = pd.read_csv('../input/employee_reviews.csv', index_col=0)
-    reviews = add_position_and_employment_status(reviews)
+    reviews = add_position_and_employment_status(reviews, 'amazon')
+

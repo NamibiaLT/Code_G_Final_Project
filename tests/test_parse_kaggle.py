5e3e98022fadaa4ac34edad66bc6b9987c0ba3c7
taylor <sptaylor56@gmail.com>
2019-01-31 10:32:11 -0800

    initial test commit for test_parse_kaggle

---
+++ tests/test_parse_kaggle.py
@@ -0,0 +1,55 @@
+import datetime as dt
+import numpy as np
+import nltk
+import pandas as pd
+from Code_G_Final_Project.reviews_app.model import parse_kaggle as pk
+import pytest
+
+from string import punctuation, digits
+
+
+def make_dummy_dataframe():
+    companies = ['amazon', 'google', 'facebook']
+    job_titles = ['Current Employee - Anonymous Employee', 'Former Employee - Software Engineer', 'Current Employee - Manager']
+    company_list = [companies[x] for x in np.random.randint(0, 2, 100)]
+    job_title_list = [job_titles[x] for x in np.random.randint(0, 2, 100)]
+    dummy_df = pd.DataFrame()
+    dummy_df['company'] = company_list
+    dummy_df['job-title'] = job_title_list
+    return dummy_df
+
+
+DUMMY_DF = make_dummy_dataframe()
+
+
+def test_add_position_and_employment_status_no_filter():
+    df = pk.add_position_and_employment_status(DUMMY_DF)
+    assert isinstance(df, pd.core.frame.DataFrame)
+    assert list(df.columns) == ['company', 'job-title', 'current_employee', 'position']
+
+
+def test_add_position_and_employment_status_single_filter():
+    df = pk.add_position_and_employment_status(DUMMY_DF, ['amazon'])
+    assert isinstance(df, pd.core.frame.DataFrame)
+    assert list(df.columns) == ['company', 'job-title', 'current_employee', 'position']
+    assert list(df['company'].unique()) == ['amazon']
+
+
+def test_add_position_and_employment_status_multiple_filter():
+    df = pk.add_position_and_employment_status(DUMMY_DF, ['amazon', 'google'])
+    assert isinstance(df, pd.core.frame.DataFrame)
+    assert list(df.columns) == ['company', 'job-title', 'current_employee', 'position']
+    assert list(df['company'].unique()) == ['amazon', 'google']
+
+
+def test_add_position_and_employment_status_assert_error():
+    """
+    Assert error is raised when company filter is not provided as a list
+    :return:
+    """
+    with pytest.raises(TypeError):
+        pk.add_position_and_employment_status(DUMMY_DF, 'amazon')
+
+
+def test_parse_datetime():
+    pass

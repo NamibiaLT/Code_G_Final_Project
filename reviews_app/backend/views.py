366880575f038eb51e453f2afc6a446c9e1556d0
NLT <NamibiaLT@users.noreply.github.com>
2019-01-31 19:40:04 -0800

    add show action template

--- reviews_app/backend/views.py
+++ reviews_app/backend/views.py
@@ -11,7 +11,7 @@ def index():
 # SHOW ACTION TEMPLATE- where we send users after they hit submit
 @backend_blueprint.route("/company_results")
 def show():
-    company_id = request.args.get("foo", type=str) # Pass in string as the argument name in get
+    company_id = request.args.get("SAMPLE", type=str) # Pass in string as the argument name in get
     data = get_company_data(company_id)
     return render_template('company_results.html', context=data)
 
